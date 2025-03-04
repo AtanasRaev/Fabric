abc443e26fca95c2140ff9d51534aef072967795
AtanasRaev <atanaspraev@gmail.com>
2025-03-04 18:40:27 +0200

    Add new ClothingPriceDTO class

---
+++ src/main/java/bg/tshirt/database/dto/clothes/ClothingPriceDTO.java
@@ -0,0 +1,29 @@
+package bg.tshirt.database.dto.clothes;
+
+import bg.tshirt.database.entity.enums.Type;
+
+public class ClothingPriceDTO {
+    private Type type;
+    private Double price;
+
+    public ClothingPriceDTO(Type type, double price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
