8038aebd72ec8b96b7bc7d3ed93e4313b5a1d96e
AtanasRaev <atanaspraev@gmail.com>
2025-03-04 18:39:30 +0200

    Add new methods for finding price by types amd updating price

--- src/main/java/bg/tshirt/database/repository/ClothingRepository.java
+++ src/main/java/bg/tshirt/database/repository/ClothingRepository.java
@@ -1,10 +1,13 @@
 package bg.tshirt.database.repository;
 
+import bg.tshirt.database.dto.clothes.ClothingDiscountPriceDTO;
+import bg.tshirt.database.dto.clothes.ClothingPriceDTO;
 import bg.tshirt.database.entity.Clothing;
 import bg.tshirt.database.entity.enums.Type;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -41,4 +44,22 @@ public interface ClothingRepository extends JpaRepository<Clothing, Long> {
 
     @Query("SELECT c.category, COUNT(c) FROM Clothing c WHERE LOWER(c.type) LIKE LOWER(:type) GROUP BY c.category")
     List<Object[]> countClothingByCategory(String type);
+
+    @Query("SELECT new bg.tshirt.database.dto.clothes.ClothingPriceDTO(c.type, c.price) " +
+            "FROM Clothing c " +
+            "WHERE c.id = (SELECT MIN(c2.id) FROM Clothing c2 WHERE c2.type = c.type) " +
+            "AND c.type IN :types " +
+            "ORDER BY c.type")
+    List<ClothingPriceDTO> findPricesForTypes(@Param("types") List<Type> types);
+
+    @Query("SELECT new bg.tshirt.database.dto.clothes.ClothingDiscountPriceDTO(c.type, c.discountPrice) " +
+            "FROM Clothing c " +
+            "WHERE c.id = (SELECT MIN(c2.id) FROM Clothing c2 WHERE c2.type = c.type) " +
+            "AND c.type IN :types " +
+            "ORDER BY c.type")
+    List<ClothingDiscountPriceDTO> findDiscountPricesForTypes(@Param("types") List<Type> types);
+
+    @Modifying
+    @Query("UPDATE Clothing c SET c.price = :price, c.discountPrice = :discountPrice WHERE LOWER(c.type) LIKE LOWER(:type)")
+    int bulkUpdatePrices(@Param("type") String type, @Param("price") double price, @Param("discountPrice") Double discountPrice);
 }
