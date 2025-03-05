9ed82ae6d330942d4a1ab8f5d22a4e1357df4dc6
AtanasRaev <atanaspraev@gmail.com>
2025-03-05 18:50:17 +0200

    Refactor

--- src/main/java/bg/tshirt/exceptions/ClothingAlreadyExistsException.java
+++ src/main/java/bg/tshirt/exceptions/ClothingAlreadyExistsException.java
@@ -4,8 +4,4 @@ public class ClothingAlreadyExistsException extends RuntimeException {
     public ClothingAlreadyExistsException(String message) {
         super(message);
     }
-
-    public ClothingAlreadyExistsException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
