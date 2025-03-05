68607fd683179f3278eca0e89657c22ee97b0f65
AtanasRaev <atanaspraev@gmail.com>
2025-03-05 18:54:02 +0200

    Removed unused method

--- src/main/java/bg/tshirt/database/repository/RefreshTokenRepository.java
+++ src/main/java/bg/tshirt/database/repository/RefreshTokenRepository.java
@@ -5,14 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByTokenId(String tokenId);
-
-    List<RefreshToken> findByUserEmail(String email);
 
     void deleteByExpiryDateBefore(Instant date);
 }
