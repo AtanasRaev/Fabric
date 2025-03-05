1b679977c236edd8b4e4348d3f13a7ba648119d9
AtanasRaev <atanaspraev@gmail.com>
2025-03-05 18:52:51 +0200

    Removed unused field

--- src/main/java/bg/tshirt/config/JwtAuthenticationFilter.java
+++ src/main/java/bg/tshirt/config/JwtAuthenticationFilter.java
@@ -7,8 +7,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,7 +22,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider tokenProvider;
     private final CustomUserDetailsService customUserDetailsService;
-    private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
 
     public JwtAuthenticationFilter(JwtTokenProvider tokenProvider,
                                    CustomUserDetailsService customUserDetailsService) {
