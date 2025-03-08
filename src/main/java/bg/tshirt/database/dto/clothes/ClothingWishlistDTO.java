72ad21cb3926d77d1bb8f54daa6278094116b1a9
AtanasRaev <atanaspraev@gmail.com>
2025-03-08 15:37:53 +0200

    Add new ClothingWishListDTO class

---
+++ src/main/java/bg/tshirt/database/dto/clothes/ClothingWishlistDTO.java
@@ -0,0 +1,13 @@
+package bg.tshirt.database.dto.clothes;
+
+public class ClothingWishlistDTO extends ClothingBaseDTO {
+    private String image;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
