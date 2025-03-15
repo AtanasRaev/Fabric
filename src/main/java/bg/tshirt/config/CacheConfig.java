c88e35c20a29a16853873098ca3ba89327edf4ed
AtanasRaev <atanaspraev@gmail.com>
2025-03-15 15:24:07 +0200

    Add cache config for econtOffices

--- src/main/java/bg/tshirt/config/CacheConfig.java
+++ src/main/java/bg/tshirt/config/CacheConfig.java
@@ -2,21 +2,33 @@ package bg.tshirt.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class CacheConfig {
+
     @Bean
     public CacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager("econtCities");
-        cacheManager.setCaffeine(Caffeine.newBuilder()
-                .expireAfterWrite(5, TimeUnit.DAYS)
-                .maximumSize(100));
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        CaffeineCache econtCitiesCache = new CaffeineCache("econtCities",
+                Caffeine.newBuilder()
+                        .expireAfterWrite(30, TimeUnit.DAYS)
+                        .maximumSize(1)
+                        .build());
+
+        CaffeineCache econtOfficesCache = new CaffeineCache("econtOffices",
+                Caffeine.newBuilder()
+                        .expireAfterWrite(10, TimeUnit.DAYS)
+                        .maximumSize(100)
+                        .build());
+
+        cacheManager.setCaches(List.of(econtCitiesCache, econtOfficesCache));
         return cacheManager;
     }
 }
-
