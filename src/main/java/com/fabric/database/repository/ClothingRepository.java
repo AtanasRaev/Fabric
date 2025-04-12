e6c51ff4d04225423e08fb2824c558dbd945b771
AtanasRaev <atanaspraev@gmail.com>
2025-04-12 16:32:40 +0300

    Refactor methods to check for selected field

--- src/main/java/com/fabric/database/repository/ClothingRepository.java
+++ src/main/java/com/fabric/database/repository/ClothingRepository.java
@@ -18,21 +18,26 @@ import java.util.Optional;
 
 @Repository
 public interface ClothingRepository extends JpaRepository<Clothing, Long> {
+    @Query("SELECT c FROM Clothing c WHERE c.id = :id AND c.selected = :selected ")
+    Optional<Clothing> findByIdSelected(@Param("id") Long id, @Param("selected") boolean selected);
+
     Optional<Clothing> findByModelAndTypeAndCategory(String model, Type type, Category category);
 
     @Query("SELECT c FROM Clothing c " +
-            "WHERE c.id IN (" +
+            "WHERE c.selected = TRUE AND c.id IN (" +
             "   SELECT MIN(c2.id) FROM Clothing c2 " +
-            "   WHERE LOWER(c2.name) LIKE LOWER(:query) OR LOWER(c2.model) LIKE LOWER(:query) " +
+            "   WHERE LOWER(c2.name) LIKE LOWER(:query) OR LOWER(c2.model) LIKE LOWER(:query) AND c2.selected = TRUE" +
             "   GROUP BY c2.model, c2.type" +
             ")")
     Page<Clothing> findByQuery(Pageable pageable, @Param("query") String query);
 
     @Query("SELECT c FROM Clothing c " +
-            "WHERE c.id IN (" +
+            "WHERE c.selected = true " +
+            "AND c.id IN (" +
             "    SELECT MIN(c2.id) FROM Clothing c2 " +
             "    WHERE (LOWER(c2.name) LIKE LOWER(:query) OR LOWER(c2.model) LIKE LOWER(:query)) " +
             "      AND LOWER(c2.type) IN (:type) " +
+            "      AND c2.selected = true " +
             "    GROUP BY c2.model, c2.type" +
             ")")
     Page<Clothing> findByQueryAndType(Pageable pageable,
@@ -40,21 +45,21 @@ public interface ClothingRepository extends JpaRepository<Clothing, Long> {
                                       @Param("type") List<String> type);
 
 
-    @Query("SELECT c FROM Clothing c WHERE LOWER(c.category) LIKE LOWER(:category)")
+    @Query("SELECT c FROM Clothing c WHERE c.selected = TRUE AND LOWER(c.category) LIKE LOWER(:category)")
     Page<Clothing> findByCategory(Pageable pageable, @Param("category") String category);
 
-    @Query("SELECT c FROM Clothing c WHERE LOWER(c.type) LIKE LOWER(:type)")
+    @Query("SELECT c FROM Clothing c WHERE c.selected = TRUE AND LOWER(c.type) LIKE LOWER(:type)")
     Page<Clothing> findByType(Pageable pageable, @Param("type") String type);
 
-    @Query("SELECT c FROM Clothing c WHERE LOWER(c.type) LIKE LOWER(:type) AND LOWER(c.category) LIKE LOWER(:category)")
+    @Query("SELECT c FROM Clothing c WHERE c.selected = true AND LOWER(c.type) LIKE LOWER(:type) AND LOWER(c.category) LIKE LOWER(:category)")
     Page<Clothing> findByTypeAndCategory(Pageable pageable, @Param("type") String type, @Param("category") String category);
 
-    @Query("SELECT c FROM Clothing c")
+    @Query("SELECT c FROM Clothing c WHERE c.selected = true")
     Page<Clothing> findAllPage(Pageable pageable);
 
     List<Clothing> findByModel(String model);
 
-    @Query("SELECT c.category FROM Clothing c WHERE LOWER(c.type) LIKE LOWER(:type) GROUP BY c.category")
+    @Query("SELECT c.category FROM Clothing c WHERE c.selected = TRUE AND LOWER(c.type) LIKE LOWER(:type) GROUP BY c.category")
     List<Category> getCategoriesByType(String type);
 
     @Query("SELECT new com.fabric.database.dto.clothes.ClothingPriceDTO(c.type, c.price) " +
@@ -78,6 +83,6 @@ public interface ClothingRepository extends JpaRepository<Clothing, Long> {
     @Query("SELECT MIN(c.discountPrice) FROM Clothing c WHERE c.type = :type")
     Double findDiscountPriceByType(Type type);
 
-    @Query("SELECT c.type, c.category FROM Clothing c GROUP BY c.type, c.category")
+    @Query("SELECT c.type, c.category FROM Clothing c WHERE c.selected = TRUE GROUP BY c.type, c.category")
     List<Object[]> findTypesAndCategories();
 }
